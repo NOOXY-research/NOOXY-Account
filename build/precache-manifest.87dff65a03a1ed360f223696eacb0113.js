1c0bea420666127031b302ad833801688412d183
yves <magneticchen@gmail.com>
2019-10-13 16:33:00 +0800

    changed directories

---
+++ build/precache-manifest.87dff65a03a1ed360f223696eacb0113.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "55d4ff12ff770b836902",
+    "url": "/static/css/main.e7d0905d.chunk.css"
+  },
+  {
+    "revision": "55d4ff12ff770b836902",
+    "url": "/static/js/main.55d4ff12.chunk.js"
+  },
+  {
+    "revision": "39a293b330aed6a9766d",
+    "url": "/static/js/1.39a293b3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8758ea3365395d97c0e5a025b361b43e",
+    "url": "/index.html"
+  }
+];
